d9a79289cca7dc9b56aa8ff2141f6f32b8f057a0
rakibulhuda <rakibulhudaa@gmail.com>
2026-02-23 10:57:10 +0600

    added tracker section

---
+++ scripts/tracker.js
@@ -0,0 +1,8 @@
+let totalJobCount = document.getElementById("totalJobCount");
+totalJobCount.innerText = 0;
+
+let interviewedCount = document.getElementById("interviewedCount");
+interviewedCount.innerText = 0;
+
+let rejectedCount = document.getElementById("rejectedCount");
+rejectedCount.innerText = 0;
